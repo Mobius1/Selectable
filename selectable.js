c8e613e855b5d70c8784744d7a017d7ce5c24a3a
Mobius1 <admin@twitchaddict.tv>
2017-11-04 15:07:49 +0000

    v0.6.2

--- selectable.js
+++ selectable.js
@@ -5,7 +5,7 @@
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
  *
- * Version: 0.6.1
+ * Version: 0.6.2
  *
  */
 (function(root, factory) {
@@ -21,7 +21,7 @@
 })(typeof global !== 'undefined' ? global : this.window || this.global, function() {
     "use strict";
 
-    var _version = "0.6.1";
+    var _version = "0.6.2";
 
     var _touch = (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
 
