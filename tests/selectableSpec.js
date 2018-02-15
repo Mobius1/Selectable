bcc750b19af50be41bed322a2802cad42ac16b16
Jan Esser <jane@inventis.be>
2018-02-15 11:30:49 +0100

    adds tests to prove current implementation does not match native behaviours
    if you look at a <select> element, those behaviours differ from the current

--- tests/selectableSpec.js
+++ tests/selectableSpec.js
@@ -58,5 +58,50 @@ describe('selectable', function () {
                 expect(container.firstChild.getAttribute('class')).toContain('test-selected');
             });
         });
+
+
+        describe('with shiftKey', function () {
+            var container, selectable;
+            beforeEach(function () {
+                document.body.innerHTML = '<ul id="selectable"><li>1</li><li>2</li><li>3</li></ul>';
+
+                container = document.getElementById('selectable');
+                selectable = new Selectable({
+                    appendTo: container,
+                    filter: 'li',
+                    multiple: true,
+                    toggle: false,
+                    classes: {
+                        selected: "test-selected"
+                    }
+                });
+            });
+            afterEach(function () {
+                selectable.unbind();
+            });
+
+            it('first time click only selects that node', function () {
+                clickNode(container.lastChild, false, true);
+                expect(container.lastChild.getAttribute('class')).toContain('test-selected');
+                expect(container.firstChild.getAttribute('class')).not.toContain('test-selected');
+                expect(container.children[1].getAttribute('class')).not.toContain('test-selected');
+            });
+
+            it('selects all rows forward', function () {
+                clickNode(container.firstChild, false, false);
+                clickNode(container.lastChild, false, true);
+                expect(container.lastChild.getAttribute('class')).toContain('test-selected');
+                expect(container.firstChild.getAttribute('class')).toContain('test-selected');
+                expect(container.children[1].getAttribute('class')).toContain('test-selected');
+            });
+
+            it('selects all rows reverse', function () {
+                clickNode(container.lastChild, false, false);
+                clickNode(container.firstChild, false, true);
+                expect(container.lastChild.getAttribute('class')).toContain('test-selected');
+                expect(container.firstChild.getAttribute('class')).toContain('test-selected');
+                expect(container.children[1].getAttribute('class')).toContain('test-selected');
+            });
+        });
     });
 });
