2376cfbe7223d0be031d0f942370bc911776d5ff
Mobius1 <admin@twitchaddict.tv>
2018-11-10 08:39:05 +0000

    Update DOCS

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -255,7 +255,10 @@ function createNavLinks() {
         const li = document.createElement("li");
         li.className = "toc-entry";
         li.innerHTML = `<a class="js-scroll-trigger" href="#${title.id}">${title.textContent}</a>`;
-        title.insertAdjacentHTML("beforeend", `<a class="header-link" href="#${title.id}"><i class="ti-link"></i></a>`);
+        
+        if ( !title.querySelector(".header-link") ) {
+            title.insertAdjacentHTML("beforeend", `<a class="header-link" href="#${title.id}"><i class="ti-link"></i></a>`);
+        }
 
         frag.appendChild(li);
     });
