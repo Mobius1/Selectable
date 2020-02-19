bb25d2ae918d3093cf3ee5cfd2020491a19212b7
Mobius1 <mobius1@gmx.com>
2020-02-19 11:50:08 +0000

    Ignore docs directory

--- selectable.js
+++ selectable.js
@@ -5,7 +5,7 @@
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
  *
- * Version: 0.17.3
+ * Version: 0.17.4
  *
  */
 (function(root, factory) {
@@ -74,7 +74,7 @@
 
     /* SELECTABLE */
     var Selectable = function(options) {
-        this.version = "0.17.3";
+        this.version = "0.17.4";
         this.v = this.version.split(".").map(s => parseInt(s, 10));
         this.touch =
             "ontouchstart" in window ||
